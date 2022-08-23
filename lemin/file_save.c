b5cd79c0d868e195a97a7e43754699995854436b
molesen <molesen@student.hive.fi>
2022-08-23 10:41:51 +0300

    save.c

--- lemin/file_save.c
+++ lemin/file_save.c
@@ -14,72 +14,72 @@
 
 /*	checks if the read line is a comment	*/
 
-static int	is_comment(char *str)
-{
-	int	i;
+// static int	is_comment(char *str)
+// {
+// 	int	i;
 
-	i = 0;
-	while (str[i] == '#')
-		++i;
-	if (i < 1)
-		return (FALSE);
-	return (i);
-}
+// 	i = 0;
+// 	while (str[i] == '#')
+// 		++i;
+// 	if (i < 1)
+// 		return (FALSE);
+// 	return (i);
+// }
 
-/*
-**	counts the amount of times the command start and end has been found
-**	start and end only can occur once
-**	the line after has to be a room name and its coordinates
-*/
+// /*
+// **	counts the amount of times the command start and end has been found
+// **	start and end only can occur once
+// **	the line after has to be a room name and its coordinates
+// */
 
-static int	first_start_or_end(char *str, int i, int *command)
-{
-	if (*command == TRUE)
-		return (ERROR);
-	if (ft_strcmp(str, "##start") == 0)
-	{
-		if (i > 1)
-			return (ERROR);
-		else
-			*command = TRUE;
-	}
-	else if (ft_strcmp(str, "##end") == 0)
-	{
-		if (i > 1)
-			return (ERROR);
-		else
-			*command = TRUE;
-	}
-	return (TRUE);
-}
+// static int	first_start_or_end(char *str, int i, int *command)
+// {
+// 	if (*command == TRUE)
+// 		return (ERROR);
+// 	if (ft_strcmp(str, "##start") == 0)
+// 	{
+// 		if (i > 1)
+// 			return (ERROR);
+// 		else
+// 			*command = TRUE;
+// 	}
+// 	else if (ft_strcmp(str, "##end") == 0)
+// 	{
+// 		if (i > 1)
+// 			return (ERROR);
+// 		else
+// 			*command = TRUE;
+// 	}
+// 	return (TRUE);
+// }
 
-/*
-**	checks if valid line
-**	valid line and their order: number of ants, coordinates, connections
-**	comments/commands can occur anywhere (also before ants?????)
-*/
+// /*
+// **	checks if valid line
+// **	valid line and their order: number of ants, coordinates, connections
+// **	comments/commands can occur anywhere (also before ants?????)
+// */
 
-static int	check_if_valid(char *str, int *i, int *total, int *command)
-{
-	if (*i == 1 && is_coordinates(str) == TRUE)
-		(*total)++;
-	else if (*i > 0 && is_connection(str) == TRUE && *total > 1)
-		(*i)++;
-	else
-	{
-		free(str);
-		str = NULL;
-		if (*i == 1)
-			return (error(COORDINATES));
-		else if (*i > 1)
-			return (error(CONNECTION));
-		else
-			return (ERROR);
-	}
-	if (*command == TRUE)
-		*command = FALSE;
-	return (TRUE);
-}
+// static int	check_if_valid(char *str, int *i, int *total, int *command)
+// {
+// 	if (*i == 1 && is_coordinates(str) == TRUE)
+// 		(*total)++;
+// 	else if (*i > 0 && is_connection(str) == TRUE && *total > 1)
+// 		(*i)++;
+// 	else
+// 	{
+// 		free(str);
+// 		str = NULL;
+// 		if (*i == 1)
+// 			return (error(COORDINATES));
+// 		else if (*i > 1)
+// 			return (error(CONNECTION));
+// 		else
+// 			return (ERROR);
+// 	}
+// 	if (*command == TRUE)
+// 		*command = FALSE;
+// 	return (TRUE);
+// }
 
 /*	saves line to input	*/
 
@@ -123,19 +123,21 @@ int	file_save(t_room *pass, char **input)
 			return (ERROR);
 		if (!line)
 			break ;
-		if (is_comment(line) >= TRUE)
-		{
-			if (first_start_or_end(line, i, &command) == ERROR)
-			{
-				free(line);
-				return (error(COMMAND));
-			}
-		}
-		else if (i == 0 && only_digits(line, &i) == TRUE)
-			pass->ants = ft_atoi(line);
-		else if (check_if_valid(line, &i, &total, &command) == ERROR)
-			return (ERROR);
+		// if (is_comment(line) >= TRUE)
+		// {
+		// 	if (first_start_or_end(line, i, &command) == ERROR)
+		// 	{
+		// 		free(line);
+		// 		return (error(COMMAND));
+		// 	}
+		// }
+		// else if (i == 0 && only_digits(line, &i) == TRUE)
+		// 	pass->ants = ft_atoi(line);
+		// else if (check_if_valid(line, &i, &total, &command) == ERROR)
+		// 	return (ERROR);
 		save_line(input, &line);
 	}
+	if (pass)
+		total = 10;
 	return (total);
 }
